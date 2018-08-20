5c6bf723501287a59357241c1b050b6aa450d9e6
teerawat <nenoteerawat@gamil.com>
2018-08-20 10:41:33 +0700

    inittal project

---
+++ src/main/groovy/com/teerawat/collector/urlcollector/UrlCollectorApplication.groovy
@@ -0,0 +1,12 @@
+package com.teerawat.collector.urlcollector
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class UrlCollectorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run UrlCollectorApplication, args
+	}
+}
---
+++ src/main/groovy/com/teerawat/collector/urlcollector/ws/DataController.groovy
@@ -0,0 +1,10 @@
+package com.teerawat.collector.urlcollector.ws
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping('/data')
+class DataController {
+	
+}
